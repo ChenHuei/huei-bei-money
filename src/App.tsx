25dc2edc971fb2c0acd3b4d7b401e5efc7b379ba
LeoChen <leo.chen@shoalter.com>
2022-01-17 12:23:47 +0800

    add material lab and date-fns plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Header from "@/components/Header";
 
 function App() {
+  const [current] = useState<Date>(new Date());
+
   return (
     <main className="flex flex-col min-w-full min-h-screen">
-      <Header />
+      <Header current={current} />
       <div className="flex-1 bg-primary">content</div>
     </main>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,21 @@
+import { format } from "date-fns";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 
 import { formatCurrency } from "@/utils/currency";
 
-interface HeaderProps {}
+interface HeaderProps {
+  current: Date;
+}
 
-function Header() {
+function Header(props: HeaderProps) {
+  const { current } = props;
+
   return (
     <header className="flex justify-between items-center px-4 py-2 bg-primaryDarkest">
-      <div />
+      <div>
+        <p>{format(current, "yyyy")}</p>
+        <p>{format(current, "MM")}</p>
+      </div>
       <div className="flex flex-col justify-center items-center">
         <div className="flex justify-center items-center w-20 h-20 p-2 rounded-full bg-secondary shadow-md">
           <div className="flex justify-center items-center w-full h-full p-2 rounded-full bg-primaryDarkest shadow-md">
@@ -18,6 +26,7 @@ function Header() {
             </div>
           </div>
         </div>
+
         <p className="mt-2 font-medium text-white">
           本月 <span>{formatCurrency(0)}</span>
         </p>
