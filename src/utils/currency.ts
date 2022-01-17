8253c414717b03e6180e2db490d6d17a3bb25259
LeoChen <leo.chen@shoalter.com>
2022-01-17 10:37:00 +0800

    add currency utils

---
+++ src/utils/currency.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
+export const formatCurrency = (number: number): string =>
+  new Intl.NumberFormat("en-UK", { style: "currency", currency: "TWD" }).format(
+    number
+  );
