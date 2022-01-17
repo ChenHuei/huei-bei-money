5173859316ead84088728c376a06ad677a6e16e3
LeoChen <leo.chen@shoalter.com>
2022-01-17 10:36:43 +0800

    update tailwind and eslint config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D3DEDC",
+        primaryDarker: "#92A9BD",
+        primaryDarkest: "#7C99AC",
+        secondary: "#FFEFEF",
+      },
+    },
   },
   plugins: [],
 };
