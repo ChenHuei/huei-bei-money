43236974e0c6da1a743db0f61a68ea415588eef3
LeoChen <leo.chen@shoalter.com>
2022-01-17 10:42:18 +0800

    add components folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import EventNoteIcon from "@mui/icons-material/EventNote";
+
+import { formatCurrency } from "@/utils/currency";
+
+interface HeaderProps {}
+
+function Header() {
+  return (
+    <header className="flex justify-between items-center px-4 py-2 bg-primaryDarkest">
+      <div />
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center w-20 h-20 p-2 rounded-full bg-secondary shadow-md">
+          <div className="flex justify-center items-center w-full h-full p-2 rounded-full bg-primaryDarkest shadow-md">
+            <div className="flex justify-center items-center w-full h-full p-2 rounded-full bg-primaryDarker shadow-md">
+              <div className="flex justify-center items-center w-full h-full p-2 rounded-full bg-white shadow-md">
+                <EventNoteIcon className="w-full h-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="mt-2 font-medium text-white">
+          本月 <span>{formatCurrency(0)}</span>
+        </p>
+      </div>
+      <div />
+    </header>
+  );
+}
+
+export default Header;
+export type { HeaderProps };
