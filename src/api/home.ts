95a48724f16e750a74616be6d05fa84775dc0a57
LeoChen <leo.chen@shoalter.com>
2022-01-24 17:25:59 +0800

    add api folder

---
+++ src/api/home.ts
@@ -0,0 +1,32 @@
+import { format } from 'date-fns';
+import { addDoc, collection, Firestore, getDocs } from 'firebase/firestore/lite';
+
+import { Category } from '@/views/Home/FormDialog';
+import { Record } from '@/views/Home/RecordList';
+
+export const getRecordApi = async (db: Firestore, time: Date): Promise<Record[]> => {
+  const snapshot = await getDocs(collection(db, 'history', format(time, 'yyyyMM'), 'record'));
+  return snapshot.docs
+    .map(
+      (item) =>
+        ({
+          id: item.id,
+          ...item.data(),
+        } as Record),
+    )
+    .sort((a, b) => b.date - a.date);
+};
+
+export const addRecordApi = (db: Firestore, data: Omit<Record, 'id'>) =>
+  addDoc(collection(db, 'history', format(data.date, 'yyyyMM'), 'record'), {
+    ...data,
+    createdBy: 'huei',
+  });
+
+export const getCategoryListApi = async (db: Firestore): Promise<Category[]> => {
+  const snapshot = await getDocs(collection(db, 'category'));
+  return snapshot.docs.map((item) => ({
+    id: item.id,
+    ...item.data(),
+  })) as Category[];
+};
