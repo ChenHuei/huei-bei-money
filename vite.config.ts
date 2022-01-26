c38e21fe7dd4985c1a106a478afac38cf1230667
LeoChen <leo.chen@shoalter.com>
2022-01-26 16:10:54 +0800

    update pwa setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,44 @@
-import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
-const path = require("path");
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-      ],
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
-        name: "Huei & Bei Money",
-        short_name: "HB Money",
-        description: "Huei & Bei Money ❤️❤️❤️",
-        theme_color: "#ffffff",
-        start_url: "/?standalone=true",
-        orientation: "portrait-primary",
+        name: 'Huei & Bei Money',
+        short_name: 'Huei & Bei ❤️❤️❤️',
+        description: 'cost record ',
+        theme_color: '#ffffff',
+        start_url: '/?standalone=true',
+        orientation: 'portrait-primary',
         icons: [
           {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
           {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
           },
         ],
       },
