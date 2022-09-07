971b50fa6d301b8338241d7751004be2d85d793f
LeoChen <leo_chen@mayohr.com>
2022-09-07 16:46:18 +0800

    refactor routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, RouteProps, Routes } from 'react-router-dom';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App';
 import './index.css';
@@ -17,22 +17,67 @@ const updateSW = registerSW({
   onOfflineReady() {},
 });
 
+type CustomRoute<T> = T & {
+  children?: CustomRoute<T>;
+};
+
+const ROUTES: CustomRoute<RouteProps[]> = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'home',
+        element: <MainLayout />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+          {
+            path: 'user',
+            element: <User />,
+          },
+          {
+            path: 'family',
+            element: <Family />,
+          },
+          {
+            path: 'chart',
+            element: <Chart />,
+          },
+        ],
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        index: true,
+        element: <Navigate to="/home" />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <Navigate to="/home" />,
+  },
+];
+
+const renderRoute = (list: CustomRoute<RouteProps[]>) =>
+  list.map((item) => {
+    const { path, element, index = false, children = [] } = item;
+    return (
+      <Route key={path} path={path} index={index} element={element}>
+        {Array.isArray(children) && children.length && renderRoute(children)}
+      </Route>
+    );
+  });
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="home" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="user" element={<User />} />
-            <Route path="family" element={<Family />} />
-            <Route path="chart" element={<Chart />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route index element={<Navigate to="/home" />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/home" />} />
-      </Routes>
+      <Routes>{renderRoute(ROUTES)}</Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
