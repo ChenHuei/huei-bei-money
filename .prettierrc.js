4ff066418f83f6264079da07f631283821b3ea29
LeoChen <leo_chen@mayohr.com>
2023-11-01 11:27:22 +0800

    add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  printWidth: 80
+};
